efd266812bf56e6dcf365fbd3ce9d1b2fec1e5c6
Alexandr Podgornov <alexandr2000vice@gmail.com>
2023-04-06 14:26:54 +0300

    add test1 v2

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ api_url = "http://localhost:8000"
 
 
 def test_healthCheck():
-    response = requests.get(f'{api_url}/__health_check')
+    response = requests.get(f'{api_url}/__health')
     assert response.status_code == 200
 
 
