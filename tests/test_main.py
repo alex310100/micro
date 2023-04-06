6263783ae62935fb446576411b029cf90d249d91
Alexandr Podgornov <alexandr2000vice@gmail.com>
2023-04-06 14:16:50 +0300

    tets1

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ api_url = "http://localhost:8000"
 
 
 def test_healthCheck():
-    response = requests.get(f'{api_url}/__health')
+    response = requests.get(f'{api_url}/__health_check')
     assert response.status_code == 200
 
 
